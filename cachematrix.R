fb656dfbc8f1390fc9cfd6c225fbfbd4e9856be6
marcosavoia <ilhamfadhil@gmail.com>
2017-08-19 18:26:32 +0700

    add function
    add new function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates an R object that stores a vector and its inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL ## initilize object m 
+  set <- function(y){ ## define setter
+    x <<- y ## to cleared memory
+    m <<- NULL ## to cleared memory
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function retreived cached object from makeCacheMatrix and 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if (!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setsolve(m)
+  m
 }
